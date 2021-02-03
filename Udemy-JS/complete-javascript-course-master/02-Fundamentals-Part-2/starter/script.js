fa44ab1a2c3ea792503f069a4b74fc11fdc1d69f
Taeing <hi2177@naver.com>
2021-02-03 23:44:53 +0900

    Study: JS-two way of function declaration

--- Udemy-JS/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,4 @@
+/*
 'use strict'; // 특정 오류가 발생했을 시, 오류 내용을 알려주고 특정 변수명을 비활성화함
 
 function logger() {
@@ -21,3 +22,24 @@ const appleOrangeJuice =fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 logger(22);
+
+// function calcAge1(birthYear){
+    //      const age = 2037 - birthYear;
+    //      return age;
+    // } 아래 함수와 같은 기능
+    
+    // Function declaration
+    function calcAge1(birthYear){
+        return 2037 - birthYear;
+    }
+    const age1 =calcAge1(1991);
+    
+    // Function expression
+    const calcAge2 = function (birthYear) { // 함수명이 없는 함수(익명함수)
+        return 2037 - birthYear;
+    }
+    const age2 = calcAge2(1991);
+    
+    console.log(age1, age2);
+
+    */
