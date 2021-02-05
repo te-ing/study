a79f7ac27a9d38eb0f59e1cf2f9206a25c55a056
Taeing <hi2177@naver.com>
2021-02-05 21:24:56 +0900

    Study: JS-udemy ~part2

--- Udemy-JS/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -170,4 +170,280 @@ console.log(friends.includes('23')); //type coercion 일어나지 않음
 if (friends.includes('Steven')) {
   console.log('피터를 생일파티에 부르자!')
 }
+
+// Coding Challenge #2
+// Steven은 여전히 50~300은 15% 그외는 20%인 팁계산기를 만들고 있다. 
+
+// Tasks:
+// 팁과 상응하는넣고 받을 수 있는 아무 값을 가진 'calcTip'함수를 써라 
+// 100을 테스트해봐라
+// 그리고 테스트 데이터를 가진 bills 이라는 배열을 써봐라 
+// 배열은 함수를 통해 계산된 가격이 담긴 tips배열을 만들어라
+// total이라는 배열을 만들어라
+
+// TEST data: 125, 555, 44
+
+function calcTip(bill){
+  if (bill <= 50 || bill >= 300){
+    return bill*0.2}
+    else return bill*0.15;
+  }
+  
+  console.log(calcTip(100));
+  
+  const bills = [125, 555, 44]
+  console.log(bills);
+  
+  const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+  console.log(tips)
+  
+  const total = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]];
+  console.log(total)
+  
+  // Object
+
+  const TJ = {
+    firstName: 'Taejung',
+    lastName: 'Kim',
+    age: ' 2021-1996',
+    job: 'student',
+    friends: ['CL', 'INO', 'PT']
+  }; // value : key(properies)
+  
+  console.log(TJ.lastName);
+  console.log(TJ['lastName']); // 콜론이 있어야함
+  
+  const nameKey = 'Name'
+  console.log(TJ['first' + nameKey]);
+  
+  // const interestedIn = prompt("나에 대해 알고 싶은 것이 있니? firstName, lasName, age, job, friends 중에 골라줘" );
+  // console.log(TJ[interestedIn]);
+
+  // if (TJ[interestedIn]) {
+    //   console.log(TJ[interestedIn]);
+  // } else {
+    //   console.log("잘못된 값 입력");
+  // }
+  
+  TJ.location = 'Korea';
+  TJ['instagram'] = '@kimtae_ing'
+  console.log(TJ);
+  
+  console.log(`${TJ['firstName']} has ${TJ.friends.length} friends, and his best friend is called ${TJ.friends[0]}`)
+  
+  const TJ = {
+    firstName: 'Taejung',
+    lastName: 'Kim',
+    birthYear: '1996',
+    job: 'student',
+    friends: ['CL', 'INO', 'PT'],
+    hasDriversLicense: true,
+    
+    // calcAge: function(birthYear) { // TJd object 내의 함수
+    //   return 2037 - birthYear
+    // }
+    
+    // calcAge: function () { // TJd object 내의 함수
+    //   console.log(this) // this : TJ의 모든 object
+    //   return 2037 - this.birthYear; //birthYear의 값
+    // }
+    
+    calcAge: function () { // TJd object 내의 함수
+      this.age = 2037 - this.birthYear;
+      return this.age; 
+    },
+    
+    getSummary: function() {
+      if(this.hasDriversLicense){
+        return `${this.firstName} is a ${this.age}-year old teacher, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`}
+      }
+    };
+    
+    console.log(TJ.calcAge());
+    console.log(TJ.age);
+    
+    
+    // const calcAge = function(birthYear) { 원래 표기법
+    //   return 2037 - birthYear
+    // }
+    
+    console.log(TJ.getSummary())
+    // const TJ = {
+//   firstName: 'Taejung',
+//   lastName: 'Kim',
+//   birthYear: '1996',
+//   job: 'student',
+//   friends: ['CL', 'INO', 'PT'],
+//   hasDriversLicense: true,
+
+
+// Coding Challenge #3
+
+// Mark와 John의 BMI지수 계산 BMI = mass / height ** 2 = mass / (height*height)
+
+// 1. 각각의 이름과 mass, height를 기입한 object를 만들어라
+// 2. calcBMI 함수를 이용해 BMI를 계산해라. BMI값을 저장하고, return하여라
+// 3. BMI가 누가 더 높은지, 이름과 BMI지수를 출력해라. ex) John's BMI(23.3)is higher than Mark's(23.9)!
+
+// Test data: Mark 78 / 1.69 John 92 / 1.95
+
+const markBMI = {
+  fullName : 'Mark Miller' ,
+  mass: '78',
+  height: '1.69',
+  calcBMI : function(){
+    return this.mass / (this.height**2)
+  }
+}
+
+const johnBMI = {
+  fullName : 'John Smith' ,
+  mass: '92',
+  height: '1.95',
+  calcBMI : function(){
+    return this.mass / (this.height**2)
+  }
+}
+
+
+console.log(markBMI.calcBMI())
+console.log(johnBMI.calcBMI())
+
+console.log(`${markBMI.fullName}'s(${markBMI.calcBMI()})is higher than ${johnBMI.fullName}'s(${johnBMI.calcBMI()})!`)
+
+// Loof
+
+// for loof keeps running while condition is TRUE
+// for(let rep = 1; rep <= 10; rep++) {
+  //   console.log(`Lifting weights repetition ${rep}`)
+  // }
+  
+  const TJ = [
+    'Taejung',
+    'Kim',
+    '1996',
+    'student',
+    ['CL', 'INO', 'PT'],
+    true
+  ];
+  const types = []
+  
+  for (let i = 0; i < TJ.length; i++) {
+    console.log(TJ[i], typeof TJ[i])
+    types.push(typeof TJ[i]);
+  }
+  
+  console.log(types);
+  
+  console.log('---ONLY STRING---');
+  // contionue and loof
+  for (let i = 0; i < TJ.length; i++) {
+    if(typeof TJ[i] !== 'string') continue; 
+    console.log(TJ[i], typeof TJ[i])
+  }
+  console.log('---ONLY BREAK---');
+  // contionue and loof
+  for (let i = 0; i < TJ.length; i++) {
+    if(typeof TJ[i] !== 'boolean') break; 
+    console.log(TJ[i], typeof TJ[i])
+  }
+  
+  
+  const TJ = [
+    'Taejung',
+    'Kim',
+    2037 - 1991,
+    'student',
+    ['CL', 'INO', 'PT'],
+  ];
+  
+  for(let i = TJ.length -1; i >= 0; i--) {
+    console.log(TJ[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-----Starting exercise ${exercise}`);
+  
+  for (let rep=1; rep<6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+  }
+}
+
+// for (let rep=1; rep<=5; rep++) {
+  //   console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+  // }
+  
+  // let rep = 1; // while문에서는 밖에서 선언해줘야 함.
+  // while(rep <=5) {
+    //   console.log(`Lifting weight repetition ${rep}`);
+    //   rep++;
+    // }
+    
+    let dice = Math.trunc(Math.random()*6) // Math.random(): 0~1 사이의 랜덤 수 Math.trunc(Math.random()) 정수
+    
+    while(dice !==5) {
+      dice = Math.trunc(Math.random()*6);
+      console.log(`You rolled a ${dice+1}`);
+    }
+    
 */
+
+// Coding Challenge #4
+
+// loop를 이용해서 Steven씨의 tip 계산기를 업그레이드 해보자!
+// 임무:
+// 1. 10개의 bill값이 담긴 bills  배열을 만들어라
+// 2. tip과 totals의 빈 배열을 만들어라
+// 3. bills의 모든 값을 calcTip함수를 이용해 팁과 값을 계산해라.(For loop사용!)
+// data: 22 295 176 440 37 105 101 100 86 52
+
+// 4.인자 arr이라 불리는 caclAverage를 사용해라. 이 함수는 array의 평균을 가진다.
+// 4.1 먼저 모든 값을 array에 추가해라. 그담 0부터 시작하는 sum변수를 넣어라
+// 그담 loop를 사용해라 각각 sum 변수에 현재값을 넣어라 모든 값을 넣어야 끝난다.
+// 4.2 평균계산을 위해 
+// 5. 함수와 함께 totals. 정렬을 사용해라
+
+let = bills = [22, 295, 176, 440, 37, 105, 101, 100 ,86, 52]
+let = tips = []
+let = totals = []
+
+console.log(bills)
+
+
+
+for (let i = 0; i < 10; i++) {
+  const calcTip = calcTip=> bills[i] >= 50 || bills[i] <= 300 ? bills[i]*0.15 : bills[i]*0.15;
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i]+tips[i]);
+}
+
+console.log(tips);
+console.log(totals);
+
+arr = {
+  bills,
+  tips,
+  totals,
+  caclAverage: function(){
+    let sum = 0;
+    for ( let j = 0; j < this.length -1; j++){
+      sum+this[j]
+  } return sum / this.length -1
+  }
+}
+
+console.log(arr.caclAverage(totals)
+
+
+// calcBMI : function(){
+//   return this.mass / (this.height**2)
+
+
+
+
+// const calcAge3 = birthYear => 2037 - birthYear; //한가지 매개 변수만 가지고 있을 때 유용
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const drink = age >= 18 ? 'wine' : 'milk🥛';
+// console.log(drink);
