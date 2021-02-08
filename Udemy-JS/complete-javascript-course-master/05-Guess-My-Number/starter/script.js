5ea1ccc5f41ab385e74784823c99c968dfb6c649
Taeing <hi2177@naver.com>
2021-02-08 20:26:36 +0900

    Study: JS-udemy ~73.

--- Udemy-JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,21 @@
-'use strict';
+"use strict";
+
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+console.log(document.querySelector(".guess").value); // .guess의 value값 가져오기
+document.querySelector(".guess").value = 23;
+*/
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value); // Number로 타입변환
+  console.log(typeof guess); // 얻게되는 값은 대부분 string
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "🚫No number!🚫";
+  }
+});
