5a0cfd83a9195915da0cd7a505592311744b0aa7
te-ing <hi2177@naver.com>
2021-02-25 23:47:34 +0900

    Project: converting-decoding-source

--- converting/index.js
+++ converting/index.js
@@ -3,15 +3,27 @@
 function printSource()  {
   const source = document.getElementById('source').value;
   const decodingSource = escape(source)
+  const composingSource = decodingSource.normalize('NFC');
+  const incodingSource = unescape(composingSource);
   document.getElementById("target").innerText = source;
   document.getElementById("decode").innerText = decodingSource;
+  document.getElementById("composedDecode").innerText = composingSource;
+  document.getElementById("result").innerText = incodingSource;
 }
 
 function secondePrintSource()  {
   const secondeSource = document.getElementById('secondeSource').value;
   const normalizeSource = secondeSource.normalize('NFD');
+  const normalizeNFCSource = secondeSource.normalize('NFC');
+  const secondeIncodingSource = unescape(normalizeNFCSource);
+
   const secondeDecodingSource = escape(secondeSource);
   const normalizeDecodingSource = escape(normalizeSource);
+  const normalizeDecodingNFCSource = escape(normalizeNFCSource);
+
   document.getElementById("secondeDecode").innerText = secondeDecodingSource;
   document.getElementById("secondeTarget").innerText = normalizeDecodingSource;
+  document.getElementById("secondeComposedTarget").innerText = normalizeDecodingNFCSource;
+  document.getElementById("secondeResult").innerText = secondeIncodingSource;
+
 }
