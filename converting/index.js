092df94bfe38dbc65216930259ed236c4e79bfcd
te-ing <hi2177@naver.com>
2021-02-25 17:43:21 +0900

    Project: converting-decoding

--- converting/index.js
+++ converting/index.js
@@ -1,9 +1,17 @@
 "use strict";
 
-function print()  {
-  const source = document.getElementsByClassName("source-text").value;
-  
-  console.log(source)
-  
+function printSource()  {
+  const source = document.getElementById('source').value;
+  const decodingSource = escape(source)
+  document.getElementById("target").innerText = source;
+  document.getElementById("decode").innerText = decodingSource;
 }
- 
+
+function secondePrintSource()  {
+  const secondeSource = document.getElementById('secondeSource').value;
+  const normalizeSource = secondeSource.normalize('NFD');
+  const secondeDecodingSource = escape(secondeSource);
+  const normalizeDecodingSource = escape(normalizeSource);
+  document.getElementById("secondeDecode").innerText = secondeDecodingSource;
+  document.getElementById("secondeTarget").innerText = normalizeDecodingSource;
+}
