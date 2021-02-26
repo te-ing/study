d71fb468332418b5fbfe2ba5db87e96a8830d39b
te-ing <hi2177@naver.com>
2021-02-26 11:23:07 +0900

    Project: convert-code complete

--- converting/index.js
+++ converting/index.js
@@ -1,29 +1,8 @@
 "use strict";
 
-function printSource()  {
-  const source = document.getElementById('source').value;
-  const decodingSource = escape(source)
-  const composingSource = decodingSource.normalize('NFC');
-  const incodingSource = unescape(composingSource);
-  document.getElementById("target").innerText = source;
-  document.getElementById("decode").innerText = decodingSource;
-  document.getElementById("composedDecode").innerText = composingSource;
-  document.getElementById("result").innerText = incodingSource;
-}
+function resultPrint()  {
+  const source = document.getElementById('source').value; // input값 가져오기
+  const sourceC = source.normalize('NFC'); // 자모결합
 
-function secondePrintSource()  {
-  const secondeSource = document.getElementById('secondeSource').value;
-  const normalizeSource = secondeSource.normalize('NFD');
-  const normalizeNFCSource = secondeSource.normalize('NFC');
-  const secondeIncodingSource = unescape(normalizeNFCSource);
-
-  const secondeDecodingSource = escape(secondeSource);
-  const normalizeDecodingSource = escape(normalizeSource);
-  const normalizeDecodingNFCSource = escape(normalizeNFCSource);
-
-  document.getElementById("secondeDecode").innerText = secondeDecodingSource;
-  document.getElementById("secondeTarget").innerText = normalizeDecodingSource;
-  document.getElementById("secondeComposedTarget").innerText = normalizeDecodingNFCSource;
-  document.getElementById("secondeResult").innerText = secondeIncodingSource;
-
+  document.getElementById("target").innerText = sourceC;
 }
