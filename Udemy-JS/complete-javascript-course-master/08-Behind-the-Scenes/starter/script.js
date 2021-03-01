82b4366fbca30def71a068fa5e3bb7cfec1ee5f2
te-ing <hi2177@naver.com>
2021-03-01 18:25:52 +0900

    Study: JS-udemy-~.97

--- Udemy-JS/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,10 +1,17 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  console.log(firstName);
-  return age;
-}
+// console.log(me);
+// console.log(job);
+// console.log(year);
 
-const firstName = 'Jonas'
-calcAge(1991);
+// var me = 'Jonas'
+// let job = 'teacher';
+// const year = 1991;
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x == window.x);
+// console.log(y == window.y); // 윈도우에 속성을 만들지 않음.
+// console.log(z == window.z);
