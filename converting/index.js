df39dda0248fe7f5b6fd9271fe99c0affa043cde
te-ing <hi2177@naver.com>
2021-03-02 21:08:18 +0900

    추가: 자모분리 사이트 복사알림 애니메이션 추가

--- converting/index.js
+++ converting/index.js
@@ -12,6 +12,7 @@ function resultPrint()  {
 document.querySelector(".copy").addEventListener("click",function () {
   const copyTarget = document.getElementById("target").innerText; //target 글자 가져오기
   const temp = document.createElement('textarea'); // textarea 라는 임시태그 생성
+  const copyNotify = document.querySelector(".copyNotify");
   document.body.appendChild(temp);
 
   temp.value = copyTarget // temp의 값은 target의 값
@@ -20,5 +21,13 @@ document.querySelector(".copy").addEventListener("click",function () {
   document.execCommand("copy"); // 선택한 값 복사
   document.body.removeChild(temp); // 임시태그 삭제
 
-  window.alert("복사가 완료되었습니다.")
+  // 복사 완료알림
+  copyNotify.classList.add("fade");
+  setTimeout(function(){
+    copyNotify.classList.remove("fade");
+  }, 1000);
+  // copyNotify.classList.remove("hidden");
+  // setTimeout(function(){
+  //   copyNotify.classList.add("hidden");
+  // }, 700);
 });
