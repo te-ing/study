5e1aecc2392e7a67d57a63207dc8d4ebacb8b182
te-ing <hi2177@naver.com>
2021-03-12 00:03:57 +0900

    Study: 자바의정석 Scanner

---
+++ JAVA/hello/src/hello/Ex_Scanner.java
@@ -0,0 +1,21 @@
+package hello;
+import java.util.*;
+
+
+// 책이랑 똑같이 했는데 왜 에러가 생기는지 모르겠음
+// 원인은 클래스명을 Scanner으로 한 것이 원인
+
+public class Ex_Scanner {
+	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		System.out.print("두자리 정수를 하나 입력해주세요: ");
+//		String input = scanner.nextLine();
+//		int num = Integer.parseInt(input);
+//		
+//		System.out.println("입력내용 : "+input);
+//		System.out.printf("num=%d%n", num);
+		
+		System.out.println((float)1.5f);
+	}
+}
---
+++ JAVA/hello/src/hello/Swap.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Swap {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 10, y = 20;
+		int tmp;
+		tmp=x;
+		x=y;
+		y=tmp;
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
