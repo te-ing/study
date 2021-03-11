b0773f81f68eaa15a7bf21aa22153092e8eb8ce4
te-ing <hi2177@naver.com>
2021-03-11 23:55:26 +0900

    Study: 자바의정석  Scanner

--- JAVA/hello/src/hello/Hello.java
+++ JAVA/hello/src/hello/Hello.java
@@ -5,7 +5,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World");
-
+		int year = 2021, age = 25;
+		int birth = year - age;
+		
+		System.out.println(birth);
 	}
 
 }
