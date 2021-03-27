7f9341bf4293a2ede909ae60945c0cc1e99affe7
te-ing <hi2177@naver.com>
2021-03-28 01:30:58 +0900

    Study: 맥도날드 클론코딩 banner 슬라이드 css

--- clonecoding_JS/clone_mcdonalds/js/banner.js
+++ clonecoding_JS/clone_mcdonalds/js/banner.js
@@ -28,5 +28,32 @@ prevBtn.addEventListener('click', function(){
   }
 });
 
+// 배너 자동재생
+let autoPlay = true
 
+setInterval(function playBar() { 
+  if(autoPlay && bannerCount < bannerNum){
+    document.querySelector('.banner').style.transform = `translateX(-${(bannerCount+1)*100}vw)`;
+    bannerCount ++;
+  }
+  else if(bannerCount = bannerNum, autoPlay){
+    bannerCount = 0;
+    document.querySelector('.banner').style.transform = `translateX(-${bannerCount*100}vw)`;
+ }
+}, 5000);
 
+// 배너 정지버튼
+const playBtn = document.querySelector('.playBtn');
+const stopBtn = document.querySelector('.stopBtn');
+
+stopBtn.addEventListener('click', function(){
+  document.querySelector('.playBar').classList.add("hidden");
+  document.querySelector('.stopBtn').classList.add("hidden");
+  autoPlay = false;
+});
+
+playBtn.addEventListener('click', function(){
+  document.querySelector('.playBar').classList.remove("hidden");
+  document.querySelector('.stopBtn').classList.remove("hidden");
+  autoPlay = true;
+});
