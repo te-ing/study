ee652220fbf71cc2288a7df2190982202b3d5a77
te-ing <hi2177@naver.com>
2021-03-29 01:20:31 +0900

    Study: 맥도날드 클론코딩 메뉴 펼치기 자바스크립트

--- clonecoding_JS/clone_mcdonalds/js/header.js
+++ clonecoding_JS/clone_mcdonalds/js/header.js
@@ -1,2 +1,13 @@
 "use strict";
 
+// 메뉴 펼치기
+const depth = document.querySelector(".depth");
+const depth2 = document.querySelector(".depth2Line");
+
+depth.addEventListener('mouseover', function(){
+  depth2.classList.remove("hidden");
+});
+
+depth2.addEventListener('mouseleave', function(){
+  depth2.classList.add("hidden");
+});
