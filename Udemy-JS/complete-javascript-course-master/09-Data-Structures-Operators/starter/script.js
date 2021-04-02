1f6db1994c89b5593a3d0134283086d6b25fbba3
te-ing <hi2177@naver.com>
2021-04-02 22:03:55 +0900

    Study: Udemy JS 101  Destructuring Arrays

--- Udemy-JS/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,18 @@ const restaurant = {
     },
   },
 };
+
+// 변수 선언
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x,y,z] = arr; //  배열이 아닌 구조화 할당 destructuring
+console.log(x, y, z);
+console.log(arr); // 원래 배열은 영향받지 않음
+
+const [first, ,second] = restaurant.categories;
+console.log(first, second);
+
+[]
