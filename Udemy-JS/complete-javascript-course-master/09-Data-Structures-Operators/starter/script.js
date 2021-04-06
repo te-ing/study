f26079c3390155d4e2560036617ef06b6cb81485
te-ing <hi2177@naver.com>
2021-04-06 22:42:16 +0900

    Study: Udemy JS 103  Destructuring object

--- Udemy-JS/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },    
+
   openingHours: {
     thu: {
       open: 12,
@@ -28,6 +32,8 @@ const restaurant = {
   },
 };
 
+/*
+// 103 객체분해
 // 변수 선언
 const arr = [2,3,4];
 const a = arr[0];
@@ -41,4 +47,26 @@ console.log(arr); // 원래 배열은 영향받지 않음
 const [first, ,second] = restaurant.categories;
 console.log(first, second);
 
-[]
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main]; // 단순한 재할당이므로 const나 let필요없음
+console.log(main, secondary);
+
+// 2개의 값을 반환하는 함수
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// 중첩된 비구조화 (destructuring)
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested;
+// console.log(i, j);
+
+const [i, ,[j,k]] = nested; // 중첩된 배열을 분해하여 작동
+console.log(i,j,k);
+
+// Default values 기본값
+const [p=1, q=1, r=1] = [8,9];
+console.log(p, q, r); // r은 정의되지 않았지만 undefined 대신 1로 출력됨
+*/
+
