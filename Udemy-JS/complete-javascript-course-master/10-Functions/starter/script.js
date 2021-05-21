d73342fd34ae44a3340aa9b6099be3f6c82dc358
te-ing <hi2177@naver.com>
2021-05-21 10:45:10 +0900

    Study: Udemy JS 127. Default Parameters

--- Udemy-JS/complete-javascript-course-master/10-Functions/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// 127. Default Parameters
+const bookings = [];
+
+const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
+  // numPassengers = numPassengers || 1; // ES5방식, undefined일 경우 1
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price
+  }
+  console.log(booking);
+  bookings.push(booking); // 함수 밖에서도 예약유지
+}
+
+createBooking('LH123')
+createBooking('LH123', 5)
+
+createBooking('LH123', undefined, 1000)
