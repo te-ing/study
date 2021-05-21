2e6ee1c0d50ea5bd0b1d8ed33208a9b94c8c7b40
te-ing <hi2177@naver.com>
2021-05-21 15:18:50 +0900

    Study: Udemy JS 129. First-Class

--- Udemy-JS/complete-javascript-course-master/10-Functions/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/10-Functions/starter/script.js
@@ -22,7 +22,6 @@ createBooking('LH123', 5)
 
 createBooking('LH123', undefined, 1000)
 
-*/
 
 // 128. How Passing Arguments Works
 
@@ -35,7 +34,7 @@ const jonas = {
 const checkIn = function(flightNum, passenger) {
   flightNum = 'LH999';
   passenger.name = 'Mr.' + passenger.name;
-
+  
   if(passenger.passport === 24739479284) {
     alert('Check in')
   } else {
@@ -60,3 +59,18 @@ newPassport(jonas);
 checkIn(flight, jonas);
 
 // 자바스크립트는 C++ 같은 언어와는 달리 함수에 대한 값(Value)은 전달하지만 레퍼런스를 전달하지 않는다.
+
+*/
+
+// 129. First-Class and Higher-Order Functions
+
+// 자바스크립트는  First-citizens(First class citizen)이다.
+
+// First-citizen 이란 투표의 자유를 가진 시민이라는 것에서 비롯된 것으로
+// 프로그래밍에서는 다른 객체들에게 적용 가능한 연산을 모두 지원하는 객체를 말한다.
+// 즉 함수에서 함수를 받거나 반환할 수 있다는 것으로, 이를 통해 고차 함수를 만들 수 있다.
+// First-class citizen 의 조건: 
+// - 변수나 데이터 구조안에 담을 수 있다.
+// - 파라미터로 전달할 수 있다.
+// - 리턴 값으로 사용할 수 있다.
+// [참고] (https://soeunlee.medium.com/javascript%EC%97%90%EC%84%9C-%EC%99%9C-%ED%95%A8%EC%88%98%EA%B0%80-1%EA%B8%89-%EA%B0%9D%EC%B2%B4%EC%9D%BC%EA%B9%8C%EC%9A%94-cc6bd2a9ecac)
