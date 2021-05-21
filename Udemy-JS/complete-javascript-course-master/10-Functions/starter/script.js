05c6aecde658e507501f4670c7823b4cd8614ba9
te-ing <hi2177@naver.com>
2021-05-21 11:55:34 +0900

    Study: Udemy JS 128. How Passing Arguments Works

--- Udemy-JS/complete-javascript-course-master/10-Functions/starter/script.js
+++ Udemy-JS/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // 127. Default Parameters
 const bookings = [];
 
@@ -20,3 +21,42 @@ createBooking('LH123')
 createBooking('LH123', 5)
 
 createBooking('LH123', undefined, 1000)
+
+*/
+
+// 128. How Passing Arguments Works
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284
+}
+
+const checkIn = function(flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if(passenger.passport === 24739479284) {
+    alert('Check in')
+  } else {
+    alert('Wrong passport!')
+  }
+}
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Is the same as doing...
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function(person) {
+  person.passport = Math.trunc(Math.random() *
+  1000000000);
+}
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+// 자바스크립트는 C++ 같은 언어와는 달리 함수에 대한 값(Value)은 전달하지만 레퍼런스를 전달하지 않는다.
