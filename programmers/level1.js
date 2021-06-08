240757b071503007422162c099b376c8cc64d7c0
te-ing <hi2177@naver.com>
2021-06-08 19:27:26 +0900

    Study: programmers level 1_체육복 테스트12 실패

--- programmers/level1.js
+++ programmers/level1.js
@@ -112,7 +112,6 @@ function solution(n, arr1, arr2){
 
 solution(5,[9, 20, 28, 18, 11],[30, 1, 21, 17, 28]);
 
-*/
 
 // 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
 
@@ -130,47 +129,115 @@ solution(5,[9, 20, 28, 18, 11],[30, 1, 21, 17, 28]);
 // commands의 각 원소는 길이가 3입니다.
 
 // function solution(array, commands) {
-
-//   let answer = [];
   
-//   function lineup(line){ 
-//     let temp;
-//     for(i=0; i<line.length; i++){
-//       for(j=0; j<line.length; j++){
-//         if(line[j]>line[j+1]){
-//           temp=line[j];
-//           line[j]=line[j+1];
-//           line[j+1]=temp;
-//         }
-//       }
-//     }
-//     return line;
-//   }
-
+  //   let answer = [];
   
-//   array1 = array.slice(commands[0][0]-1,commands[0][1]);
-//   array2 = array.slice(commands[1][0]-1,commands[1][1]);
-//   array3 = lineup(array.slice(commands[2][0]-1,commands[2][1]));
+  //   function lineup(line){ 
+    //     let temp;
+    //     for(i=0; i<line.length; i++){
+      //       for(j=0; j<line.length; j++){
+        //         if(line[j]>line[j+1]){
+          //           temp=line[j];
+          //           line[j]=line[j+1];
+          //           line[j+1]=temp;
+          //         }
+          //       }
+          //     }
+          //     return line;
+          //   }
+          
+          
+          //   array1 = array.slice(commands[0][0]-1,commands[0][1]);
+          //   array2 = array.slice(commands[1][0]-1,commands[1][1]);
+          //   array3 = lineup(array.slice(commands[2][0]-1,commands[2][1]));
+          
+          //   answer = [lineup(array1)[commands[0][2]-1], lineup(array2)[commands[1][2]-1], lineup(array3)[commands[2][2]-1]]
+          
+          //   return answer;
+          // }
+          
+          // solution(array,commands);
+          
+          const array = [1, 5, 2, 6, 3, 7, 4];
+          const commands =[[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+          
+          function solution(array, commands) {
+            var answer = [];
+            
+            for (i=0; i<commands.length; i++){
+              answer.push(array.slice(commands[i][0]-1,commands[i][1]).sort(function(a,b) {
+                return a - b;
+              })[commands[i][2]-1]);
+            }
+            return answer;
+          }
+          
+          solution(array,commands)
+          
+*/
 
-//   answer = [lineup(array1)[commands[0][2]-1], lineup(array2)[commands[1][2]-1], lineup(array3)[commands[2][2]-1]]
+// 체육복
 
+// 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+
+// 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+
+// 제한사항
+// 전체 학생의 수는 2명 이상 30명 이하입니다.
+// 체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+// 여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+// 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+// 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+
+// n	lost	reserve	return 입출력 예시
+// 5	[2, 4]	[1, 3, 5]	5
+// 5	[2, 4]	[3]	4
+// 3	[3]	[1]	2
+
+function solution(n, lost, reserve) { //테스트 12번 실패, 다른 방법 찾아야함
+  var answer = n - lost.length;
+  
+  for(lostS of lost){
+      if (reserve.includes(lostS)) {
+          answer ++;
+          reserve.splice(reserve.indexOf(lostS),1);
+          }
+      else if (reserve.includes(lostS-1)) {
+          answer ++;
+          reserve.splice(reserve.indexOf(lostS-1),1);
+          }
+      else if (reserve.includes(lostS+1)) {
+          answer ++;
+          reserve.splice(reserve.indexOf(lostS+1),1);
+          }
+       }
+  return answer;
+}
+  
+
+console.log(solution(10,[3,9,10],[3,8,9]));
+
+// 아래는 틀렸던 코드, 주의점 : 체육복 도난당한 사람은 다른사람 안빌려줌
+// function solution(n, lost, reserve) {
+//   var answer = n - lost.length;
+  
+//   for(lostS of lost){
+//       if (reserve.includes(lostS-1) || reserve.includes(lostS) || reserve.includes(lostS+1)) {
+//           answer ++;
+//           reserve.splice(reserve.indexOf(lostS),1);
+          
+//           }
+//        }
 //   return answer;
 // }
 
-// solution(array,commands);
-
-const array = [1, 5, 2, 6, 3, 7, 4];
-const commands =[[2, 5, 3], [4, 4, 1], [1, 7, 3]];
-
-function solution(array, commands) {
-  var answer = [];
-
-  for (i=0; i<commands.length; i++){
-    answer.push(array.slice(commands[i][0]-1,commands[i][1]).sort(function(a,b) {
-      return a - b;
-    })[commands[i][2]-1]);
-  }
-  return answer;
-}
-
-solution(array,commands)
+// arr = [1,2,3,4,5]
+// // 1 있는지 확인
+// console.log(arr.indexOf(1)!==-1); 
+// console.log(arr.includes(1));
+// arr.splice(arr.indexOf(1),1); // 1 삭제
+// arr.slice(1,2); // arr 배열의 [1]부터 [2]까지 슬라이스
+// arr.sort() // 아즈키코드 오름차순 정렬
+// arr.sort(function(a,b){ // 숫자 오름차순 정렬
+//   retrun a - b;
+// });
