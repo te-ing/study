951ed7e49f9bc9921f9bd86d3eb158f58c72d959
te-ing <hi2177@naver.com>
2021-07-02 15:16:21 +0900

    Study: 제로초 React 기본강좌 2-10 끝말잇기 작성

--- zeroChoReact/part2/WordRelay.jsx
+++ zeroChoReact/part2/WordRelay.jsx
@@ -1,13 +1,36 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef} = React; // React.useState('태중') 을 useState('태중')으로 줄일 수 있음
 
-class WordRelay extends React.Component {
-  state = {
-    text: 'Hello, webpack',
-  };
-  render() {
-    return <h1>{this.state.text}</h1>
+const WordRelay = () => {
+  const [word, setWord] = useState('태중');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+
+  const onChangeInput = (e) =>setValue(e.target.value);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length-1]===value[0]) {
+      setResult('정답입니다');
+      setWord(value);
+      setValue('');
+    }
+    else {
+      setResult('틀렸습니다');
+      setValue('');
+    }
   }
+
+  return( 
+    <>
+      <div>{word}</div>
+      <form onSubmit = {onSubmitForm}>
+        <input  onChange = {onChangeInput} value = {value} />
+        <button>입력</button>
+      </form>
+      <div>{result}</div>
+    </>
+  )
 }
 
 module.exports = WordRelay; // client.jsx에서 불러올 수 있도록 만들어줌
--- zeroChoReact/part2/webpack.config.js
+++ zeroChoReact/part2/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path'); // 노드에서 경로를 설정
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   name: 'WordRelay-setting', // 꼭 필요한 것은 아니나 파일 설명용
@@ -16,16 +17,31 @@ module.exports = {
     rules: [{
       test: /\.jsx?/, // js와 jsx파일에 바벨로더를 적용
       loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env', '@babel/preset-react']
+      options: { // 바벨로더의 옵션
+        presets: [
+          ['@babel/preset-env', {
+          targets: { // presets의 옵션을 설정하는 법
+            browsers: ['last 2 chrome versions', '> 5% in KR'], // '> 5% in KR' 한국에서 브라우저 점유율이 5% 이상인 브라우저를 모두 지원 browserslist
+          }
+        }],
+          '@babel/preset-react'], 
+          plugins: [
+            'react-refresh/babel'
+          ],
       }
     }],
   },
-
+  plugins: [
+    new RefreshWebpackPlugin()
+  ],
   output: { // 출력
     path: path.join(__dirname, 'dist'), // 노드에서 경로 탐색, 설정
     filename: 'app.js'
   }, 
+  devServer: {
+    publicPath: '/dist/',
+    hot: true // 핫리로딩은 기존 데이터를 유지하면서 새로고침. 핫 리로딩이 아니면 새로고침하면서 페이지 정보가 날아감
+  }
 }; // 작성 후 터미널에 webpack 타이핑
 // 'webpack'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는 배치 파일이 아닙니다.
 // 뜰 경우 1. 명령어로 등록하거나 
