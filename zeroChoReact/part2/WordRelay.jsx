8f778c86ab775d852297fa65669f0f7d9ea268cb
te-ing <hi2177@naver.com>
2021-07-02 10:34:22 +0900

    Study: 제로초 React 기본강좌 2-5 웹팩빌드

--- zeroChoReact/part2/WordRelay.jsx
+++ zeroChoReact/part2/WordRelay.jsx
@@ -3,10 +3,10 @@ const { Component } = React;
 
 class WordRelay extends React.Component {
   state = {
-
+    text: 'Hello, webpack',
   };
   render() {
-    
+    return <h1>{this.state.text}</h1>
   }
 }
 
