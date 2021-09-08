c63999fa9597e4dea63eca5a0ec7e5d2170d5756
Taejung Kim <hi2177@naver.com>
2021-09-08 19:04:12 +0900

    day28 intersection observer 방식

--- DevCourse/week6/9.8/src/PhotoList.js
+++ DevCourse/week6/9.8/src/PhotoList.js
@@ -5,6 +5,21 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
   $target.appendChild($photoList)
   this.state = initialState
 
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && !this.state.isLoading) {
+        console.log('페이지 끝', entry);
+        if(this.state.totalCount > this.state.photos.length) {
+          onScrollEnded()
+        }
+      }
+    })
+  }, {
+    threshold: 0.5   // 2번째 인자는 설정값. threshold는 마지막 콘텐츠가 얼만큼 보여졌을 때 추가데이터를 받는지
+  })
+
+  let $lastLi = null
+
   this.setState = nextState => {
     this.state = nextState
     this.render()
@@ -27,12 +42,22 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
       if ($photos.querySelector(`li[data-id="${photo.id}"]`) === null) {
         const $li = document.createElement('li')
         $li.setAttribute('data-id', photo.id)
-        $li.style = 'list-style: none;'
+        $li.style = 'list-style: none; min-height: 500px;'
         $li.innerHTML = `<img width="100%" src="${photo.imagePath}" />`
         
         $photos.appendChild($li)
       }
     })
+
+    const $nextLi = $photos.querySelector('li:last-child')
+  
+    if($nextLi !== null) {
+      if ($lastLi !== null) {
+        observer.unobserve($lastLi)
+      }
+      $lastLi = $nextLi
+      observer.observe($lastLi)
+    }
   }
 
   this.render()
