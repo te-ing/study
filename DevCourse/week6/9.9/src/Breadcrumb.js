c2e7cbc92067e1bacdf4e15d8a0c198a80213a33
Taejung Kim <hi2177@naver.com>
2021-09-09 18:08:48 +0900

    day29 고양이 사진첩

---
+++ DevCourse/week6/9.9/src/Breadcrumb.js
@@ -0,0 +1,28 @@
+export default function Breadcrumb ({ $target, initialState, onClick }) {
+  const $breadcrumb = document.createElement('nav')
+  $breadcrumb.className = 'Breadcrumb'
+  $target.appendChild($breadcrumb)
+
+  this.state = initialState
+
+  this.setState = (nextState) => {
+    this.state = nextState
+    this.render()
+  }
+
+  this.render = () => {
+    $breadcrumb.innerHTML = `
+      <div class="Breadcrumb__item">Root</div>
+      ${this.state.map(({ id, name }) => `
+        <div class="Breadcrumb__item" data-id="${id}">${name}</div>
+      `).join('')}
+    `
+  }
+  this.render()
+
+  $breadcrumb.addEventListener('click', (e) => {
+    const $breadcrumbItem = e.target.closest('.Breadcrumb__item')
+    const { id } = $breadcrumbItem.dataset
+    onClick(id)
+  })
+}
---
+++ DevCourse/week6/9.9/src/Loading.js
@@ -0,0 +1,24 @@
+export default function Loading ({ $target }) {
+
+  const $loading = document.createElement('div')
+  $loading.className = 'Loading'
+
+  $target.appendChild($loading)
+  this.state = false
+
+  this.setState = (nextState) => {
+    this.state = nextState
+    this.render()
+  }
+
+  this.render = () => {
+    $loading.innerHTML = `
+      <div class="content Modal">
+        <img width="100%;"src="https://cdn.roto.codes/images/nyan-cat.gif" alt="Loading..." />
+      </div>
+    `
+    $loading.style.display = this.state ? 'block' : 'none'
+  }
+
+  this.render()
+}
