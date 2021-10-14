3d7404e4f0355dd672660f361153dfff11f2ba9d
Taejung Kim <hi2177@naver.com>
2021-10-14 12:47:52 +0900

    데브코스 day46 컴포넌트

---
+++ DevCourse/week9/component/src/components/Breadcrumb/BreadcrumbItem.js
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled'
+import Text from '../Text'
+import Icon from '../Icon'
+
+const BreadcrumbItemContainer = styled.div`
+  display: inline-flex;
+  align-items: center;
+`
+
+const Anchor = styled.a`
+  color: inherit;
+  text-decoration: none;
+`
+
+const BreadcrumbItem = ({ children, href, active, __TYPE, ...props }) => {
+  return (
+    <BreadcrumbItemContainer {...props}>
+      <Anchor href={href}>
+        <Text size={14} strong={active}>
+          {children}
+        </Text>
+      </Anchor>
+      {!active &&
+        <Icon name="chevron-right" size={20} strokeWidth={1} />}
+    </BreadcrumbItemContainer>
+  )
+}
+
+BreadcrumbItem.defaultProps = {
+  __TYPE: 'BreadcrumbItem'
+}
+
+BreadcrumbItem.defaultProps = {
+  __TYPE: 'BreadcrumbItem'
+}
+
+export default BreadcrumbItem
---
+++ DevCourse/week9/component/src/components/Flux/index.js
@@ -0,0 +1,9 @@
+import Col from "./Col";
+import Row from "./Row";
+
+const Flux = {
+  Row,
+  Col
+}
+
+export default Flux
