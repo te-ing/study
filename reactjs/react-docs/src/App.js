cc365a78f8d3de490d552a5427db384baea769c6
Taejung Kim <hi2177@naver.com>
2021-11-22 22:09:06 +0900

    ✨ feature: 리액트 연습을 위한 기본 CRA 템플릿

---
+++ reactjs/react-docs/src/App.js
@@ -0,0 +1,12 @@
+import Practice from "./components/Practice";
+
+function App() {
+  return (
+    <div>
+      <Practice />
+      <h2>It is </h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjs/react-docs/src/components/Practice/index.js
@@ -0,0 +1,5 @@
+function Practice() {
+  return <div></div>;
+}
+
+export default Practice;
