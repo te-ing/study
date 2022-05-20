bc6aaf66f1af8fa9dbaaf57030d1bd67c0c9a1dd
Taejung Kim <hi2177@naver.com>
2022-05-20 17:32:26 +0900

    study: 리팩토링 비디오대여점 예시 추가

---
+++ Refactoring/ex1/receipt.js
@@ -0,0 +1,50 @@
+import playsData from "./plays.json" assert{ type: "json"};
+import invoicesData from "./invoices.json" assert{ type: "json"};
+
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat("en-US", { // Intl.NumberFormat 숫자 서식에 맞춰 ","를 삽입하는 함수
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (perf.audience > 30) {
+          result += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장루: ${play.type}`);
+    }
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역을 출력한다.
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n}`;
+  return result;
+}
+console.log(statement(...invoicesData, playsData));
